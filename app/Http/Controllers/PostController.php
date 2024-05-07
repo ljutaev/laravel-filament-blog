9d0f5e3fd948c19c6c974f140743582bc3e9b8f5
Sasha <ljutaev@gmai.com>
2024-05-07 20:48:44 +0300

    inner post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -46,7 +46,27 @@ class PostController extends Controller
      */
     public function show(Post $post)
     {
-        //
+        if (!$post->active || $post->published_at > Carbon::now()) {
+            throw new NotFoundHttpException();
+        }
+
+        $next = Post::query()
+            ->where('active', '=', '1')
+            ->whereDate('published_at', '<=', Carbon::now())
+            ->where('published_at', '<', $post->published_at)
+            ->orderBy('published_at', 'desc')
+            ->limit(1)
+            ->first();
+
+        $prev = Post::query()
+            ->where('active', '=', '1')
+            ->whereDate('published_at', '<', Carbon::now())
+            ->where('published_at', '>', $post->published_at)
+            ->orderBy('published_at', 'asc')
+            ->limit(1)
+            ->first();
+
+        return view('post.view', compact('post', 'next', 'prev'));
     }
 
     /**
--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,6 +1,6 @@
 <article class="bg-white    flex flex-col shadow my-4">
     <!-- Article Image -->
-    <a href="" class="hover:opacity-75">
+    <a href="{{route('view', $post)}}" class="hover:opacity-75">
         <img src="{{$post->getThumbnail()}}" alt="{{$post->title}}" class="aspect-[4/3] object-contain">
     </a>
     <div class="bg-white flex flex-col justify-start p-6">
@@ -21,7 +21,7 @@
         <a href="" class="pb-6">
             {{$post->shortBody()}}
         </a>
-        <a  href="" class="uppercase text-gray-800 hover:text-black">Continue Reading <i
+        <a  href="{{route('view', $post)}}" class="uppercase text-gray-800 hover:text-black">Continue Reading <i
                 class="fas fa-arrow-right"></i></a>
     </div>
 </article>
