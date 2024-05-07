0966a6756b77acf551b147fe90dcda966c625787
Sasha <ljutaev@gmai.com>
2024-05-07 20:19:42 +0300

    text widget

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,8 +17,16 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->realText(50);
+
         return [
-            //
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'thumbnail' => fake()->imageUrl,
+            'body' => fake()->realText(5000),
+            'active' => fake()->boolean,
+            'published_at' => fake()->dateTime,
+            'user_id' => 1,
         ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\PostController::class, 'index'])->name('home');
+Route::get('/{post:slug}', [App\Http\Controllers\PostController::class, 'show'])->name('view');
